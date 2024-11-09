b053320f99a1568bd658367fca425adfe1d74d5b
AndersonXavier-a <anderson.silva@gx2.com.br>
2024-11-09 14:39:16 -0300

    Incluindo projeto
    (cherry picked from commit b201537d9045a3c64dc4306a2079652e006df1c7)

--- src/service/service_user.py
+++ src/service/service_user.py
@@ -7,27 +7,43 @@ class ServiceUser:
         self.store = Store()
 
     def add_user(self, name, job):
-        # Verificar se os parâmetros são strings, não são vazios e contêm apenas letras
         if isinstance(name, str) and isinstance(job, str) and name.strip() != "" and job.strip() != "" and name.isalpha() and job.isalpha():
-            # Verificar se o nome já existe para adicionar
             if any(user.name == name for user in self.store.bd):
                 return "Usuário já existe"
 
-            # Adicionar usuário
             user = User(name, job)
             self.store.bd.append(user)
             return "Usuário adicionado"
         else:
-            return "Parâmetros inválidos"  # Retorno caso name ou job sejam inválidos
+            return "Parâmetros inválidos"
 
     def remove_user(self, name):
-        # Verificar se o parâmetro é string, não é vazio e contém apenas letras
         if isinstance(name, str) and name.strip() != "" and name.isalpha():
-            # Verificar se o nome existe para remover
             for user in self.store.bd:
                 if user.name == name:
                     self.store.bd.remove(user)
                     return "Usuário removido"
             return "Usuário não encontrado"
         else:
-            return "Parâmetro inválido"  # Retorno caso name seja inválido
+            return "Parâmetro inválido"
+
+    def update_user(self, name, new_job):
+        if isinstance(name, str) and isinstance(new_job,
+                                                str) and name.strip() != "" and new_job.strip() != "" and name.isalpha() and new_job.isalpha():
+
+            for user in self.store.bd:
+                if user.name == name:
+                    user.job = new_job
+                    return "Usuário atualizado"
+            return "Usuário não encontrado"
+        else:
+            return "Parâmetros inválidos"
+
+    def get_user_by_name(self, name):
+        if isinstance(name, str) and name.strip() != "" and name.isalpha():
+            for user in self.store.bd:
+                if user.name == name:
+                    return user
+            return "Usuário não encontrado"
+        else:
+            return "Parâmetro inválido"
